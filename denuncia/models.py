bd97a9d7c454f0603230fcfae40bdbf3df572dc8
Wallace <wlcs@ic.ufal.br>
2024-02-01 18:14:02 -0300

    model de denunca agora tem choices (sim ou não)

--- denuncia/models.py
+++ denuncia/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+
+CHOICES = (
+        ('yes', 'Sim'),
+        ('no', 'Não'),
+    )
+
 class Denuncia(models.Model):
+        
     relato = models.CharField(max_length = 500)
     lugar = models.CharField(max_length = 50)
-    v_fisica = models.CharField(max_length = 10)
-    v_verbal = models.CharField(max_length = 10)
-    bullying = models.CharField(max_length = 10)
-    assedio =  models.CharField(max_length = 10)
+    v_fisica = models.CharField(max_length = 10, choices=CHOICES)
+    v_verbal = models.CharField(max_length = 10, choices=CHOICES)
+    bullying = models.CharField(max_length = 10, choices=CHOICES)
+    assedio =  models.CharField(max_length = 10, choices=CHOICES)
     recorrencia = models.CharField(max_length = 50)
     data_ocorrido = models.DateTimeField()
     
