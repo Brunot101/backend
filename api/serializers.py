4ffa269a8e704ec5f9b124a6a34f87a7fa4c5280
Wallace <wlcs@ic.ufal.br>
2024-02-01 13:33:33 -0300

    apis do app denuncia na pasta denuncia, psycopg-bin, venv excluida do repo

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from denuncia.models import Denuncia
-
-
-class DenunciaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Denuncia
-        fields = '__all__'
