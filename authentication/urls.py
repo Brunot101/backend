03b5ee8dba2a9a21221b0e023ac5e13ad01c81cc
Wallace <wlcs@ic.ufal.br>
2024-02-03 11:55:00 -0300

    urls de refresh token e verify token implementadas

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from rest_framework_simplejwt import views
 
 urlpatterns = [
     path('authentication/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
+    path('authentication/token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('authentication/token/verify/', views.TokenVerifyView.as_view(), name='token_verify'),
 ]
