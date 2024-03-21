2456bd4776b2e8e1b455f70bb378a93283602741
Wallace <wlcs@ic.ufal.br>
2024-03-21 19:27:37 -0300

    fix signal

--- notification/apps.py
+++ notification/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notification'
+    
+    def ready(self):
+        # Implicitly connect signal handlers decorated with @receiver.
+        from . import signals
--- notification/signals.py
+++ notification/signals.py
@@ -4,14 +4,19 @@ from django.dispatch import receiver
 from medida_tomada.models import MedidaTomada
 from alunos.models import Aluno
 from .models import Notification
+from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save, sender=MedidaTomada)
 def create_notification_on_medidatomada_creation(sender, instance, created, **kwargs):
     if created:
-        denuncia = instance.denuncia_id
-        matricula_aluno = denuncia.matricula
-        responsaveis_aluno = Aluno.objects.get(matricula=matricula_aluno).responsaveis.all()
+        try:
+            denuncia = instance.denuncia_id
+            matricula_aluno = denuncia.matricula
+            responsaveis_aluno = Aluno.objects.get(matricula=matricula_aluno).responsaveis.all()
 
-        for responsavel in responsaveis_aluno:
-            message = f'Nova medida relacionada a um de seus dependentes foi tomada: {instance.acao}'
-            Notification.objects.create(user=responsavel.user, medida_tomada=instance, message=message)
+            for responsavel in responsaveis_aluno:
+                message = f'Nova medida relacionada a um de seus dependentes foi tomada: {instance.acao}'
+                Notification.objects.create(user=responsavel.user, medida_tomada=instance, message=message)
+        except ObjectDoesNotExist:
+            # Não faça nada se o objeto Aluno não for encontrado
+            pass
