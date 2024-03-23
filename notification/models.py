9c714761370417bc89357bba7d34d641c30efe50
Wallace <wlcs@ic.ufal.br>
2024-03-23 19:17:23 -0300

    fixing notifications para denuncia e medida tomada

--- notification/models.py
+++ notification/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from medida_tomada.models import MedidaTomada
+from denuncia.models import Denuncia
 
 # Create your models here.
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    medida_tomada = models.ForeignKey(MedidaTomada, on_delete=models.CASCADE, related_name='notifications')
+    medida_tomada = models.ForeignKey(MedidaTomada, on_delete=models.CASCADE, related_name='notifications', blank=True, null = True)
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE, related_name='notifications', blank=True, null = True)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
--- notification/signals.py
+++ notification/signals.py
@@ -1,22 +1,39 @@
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, m2m_changed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from medida_tomada.models import MedidaTomada
 from alunos.models import Aluno
+from denuncia.models import Denuncia
 from .models import Notification
 from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save, sender=MedidaTomada)
 def create_notification_on_medidatomada_creation(sender, instance, created, **kwargs):
     if created:
-        try:
-            denuncia = instance.denuncia_id
-            matricula_aluno = denuncia.matricula
-            responsaveis_aluno = Aluno.objects.get(matricula=matricula_aluno).responsaveis.all()
+        denuncia = instance.denuncia_id
+        responsaveis = set()
 
-            for responsavel in responsaveis_aluno:
-                message = f'Nova medida relacionada a um de seus dependentes foi tomada: {instance.acao}'
-                Notification.objects.create(user=responsavel.user, medida_tomada=instance, message=message)
-        except ObjectDoesNotExist:
-            # Não faça nada se o objeto Aluno não for encontrado
-            pass
+        # Adicione os responsáveis dos alunos envolvidos na denúncia aos responsáveis
+        responsaveis.update(denuncia.vitimas.all().values_list('responsaveis__user', flat=True))
+        responsaveis.update(denuncia.praticantes.all().values_list('responsaveis__user', flat=True))
+        users = User.objects.filter(id__in=responsaveis)
+        # Crie uma notificação para cada responsável
+        for user in users:
+            message = f'Nova medida tomada criada para a denúncia "{denuncia.titulo}": {instance.acao}'
+            Notification.objects.create(user=user, medida_tomada=instance, message=message)
+
+@receiver(m2m_changed, sender=Denuncia.vitimas.through)
+@receiver(m2m_changed, sender=Denuncia.praticantes.through)
+def create_notification_on_denuncia_association_change(sender, instance, action, model, pk_set, **kwargs):
+    if action == 'post_add':
+        responsaveis = set()
+
+        # Obter os responsáveis dos alunos recém-adicionados
+        for aluno_id in pk_set:
+            aluno = model.objects.get(id=aluno_id)
+            responsaveis.update(aluno.responsaveis.all())
+
+        # Criar uma notificação para cada responsável dos novos alunos adicionados
+        for responsavel in responsaveis:
+            message = f'Um dependente seu está envolvido em uma nova denúncia: {instance.titulo}'
+            Notification.objects.create(user=responsavel.user, denuncia=instance, message=message)
