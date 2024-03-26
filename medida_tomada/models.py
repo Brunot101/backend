82ddbbe7fb8ebe583f24b6d5a919bb2607d421df
Wallace <wlcs@ic.ufal.br>
2024-03-26 09:21:29 -0300

    tipo_acao medidatomada com 100 caracteres

--- medida_tomada/models.py
+++ medida_tomada/models.py
@@ -7,5 +7,5 @@ from denuncia.models import Denuncia
 class MedidaTomada(models.Model):
    
     acao = models.CharField(max_length = 200, blank=True, null=True)
-    tipo_acao = models.CharField(max_length = 30, blank=True, null=True)
+    tipo_acao = models.CharField(max_length = 100, blank=True, null=True)
     denuncia_id = models.ForeignKey(Denuncia, on_delete=models.CASCADE)
